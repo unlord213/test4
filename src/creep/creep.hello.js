6efa364dfb02824ea46a5fa5fd0d526f95e3d0d5
Unlord213 <you@example.com>
2017-05-23 21:07:21 -0500

    wip

---
+++ src/creep/creep.hello.js
@@ -0,0 +1,5 @@
+console.log('load hello')
+
+Creep.prototype.sayHello = function() {
+    console.log(this.name + " says: Hello Sunshine");
+};
--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
+require('creep/creep.hello');
 
+module.exports.loop = function() {
+    console.log('loop');
+    _.forIn(Game.creeps, (creep, creepName) => {
+        creep.sayHello();
+    });
+};
